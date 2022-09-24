b2d575029cceb560f91ab6141189458f9850d2d0
mjh9131 <mjh9131@g.rit.edu>
2022-09-24 17:27:21 -0400

    pushin it

--- ez-homework-4-script.sh
+++ ez-homework-4-script.sh
@@ -2,13 +2,15 @@
 
 VpcId='aws ec2 create-vpc --cidr-block 10.0.0.0/24 --query Vpc.VpcId --output text'
 
-aws ec2 create-subnet --vpc-id $(VpcId) --cidr-block 10.0.0.0/26
+export VpcId=$VpcId
 
-aws ec2 create-subnet --vpc-id $(VpcId) --cidr-block 10.0.0.64/26
+aws ec2 create-subnet --vpc-id "$VpcId" --cidr-block 10.0.0.0/26
 
-aws ec2 create-subnet --vpc-id $(VpcId) --cidr-block 10.0.0.128/26
+aws ec2 create-subnet --vpc-id "$VpcId" --cidr-block 10.0.0.64/26
 
-aws ec2 create-subnet --vpc-id $(VpcId) --cidr-block 10.0.0.192/26
+aws ec2 create-subnet --vpc-id "$VpcId" --cidr-block 10.0.0.128/26
+
+aws ec2 create-subnet --vpc-id "$VpcId" --cidr-block 10.0.0.192/26
 
 
 
