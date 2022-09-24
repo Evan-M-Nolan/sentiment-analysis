3df929f555e34c9cb70b29b4cb840d70ba99eb9d
mjh9131 <mjh9131@g.rit.edu>
2022-09-24 18:06:19 -0400

    Step 1 and 2 completed

--- ez-homework-4-script.sh
+++ ez-homework-4-script.sh
@@ -4,7 +4,7 @@ VPC_ID='aws ec2 create-vpc --cidr-block 10.0.0.0/24 --query Vpc.VpcId --output t
 
 export VPC_ID=$VPC_ID
 
-aws ec2 create-subnet --vpc-id "$VPC_ID" --cidr-block 10.0.0.0/26
+SUB_A=$(aws ec2 create-subnet --vpc-id "$VPC_ID" --cidr-block 10.0.0.0/26 --query Subnet.SubnetId --output text)
 
 aws ec2 create-subnet --vpc-id "$VPC_ID" --cidr-block 10.0.0.64/26
 
@@ -14,5 +14,16 @@ aws ec2 create-subnet --vpc-id "$VPC_ID" --cidr-block 10.0.0.192/26
 
 GATE_ID='aws ec2 create-internet-gateway --query InternetGateway.InternetGatewayId --output text'
 
-aws ec2 attach-internet-gateway --vpc-id vpc-2f09a348 --internet-gateway-id $GATE_ID
+aws ec2 attach-internet-gateway --vpc-id "$VPC_ID" --internet-gateway-id "$GATE_ID"
+
+TABLE_ID=$(aws ec2 create-route-table --vpc-id "$VPC_ID" --query RouteTable.RouteTableId --output text)
+
+aws ec2 create-route --route-table-id "$TABLE_ID" --destination-cidr-block 0.0.0.0/0 --gateway-id "$GATE_ID"
+
+aws ec2 describe-route-tables --route-table-id rtb-c1c8faa6 | cat
+
+aws ec2 associate-route-table  --subnet-id "$SUB_A" --route-table-id "$TABLE_ID"
+
+aws ec2 modify-subnet-attribute --subnet-id "$SUB_A" --map-public-ip-on-launch
+
 
