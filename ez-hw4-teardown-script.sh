d931164baad0f716b58ae08abf6d3257ce926455
Evan Nolan <evannolanwork@gmail.com>
2022-09-24 18:20:52 -0400

    Tear down script complete not tested

--- ez-hw4-teardown-script.sh
+++ ez-hw4-teardown-script.sh
@@ -1,2 +1,14 @@
 #! /bin/bash
-echo "The vpc_id is $VPC_ID"
+aws ec2 delete-security-group --group-id $GROUP_ID
+
+aws ec2 delete-subnet --subnet-id $SUB_A
+
+aws ec2 delete-subnet --subnet-id $SUB_B
+
+aws ec2 delete-route-table --route-table-id $ROUTE_TABLE_ID
+
+aws ec2 detach-internet-gateway --internet-gateway-id $INTERNET_GATEWAY_ID --vpc_id $VPC_ID
+
+aws ec2 delete-internet-gateway --internet-gateway-id $INTERNET_GATEWAY_ID
+
+aws ec2 delete-vpc --vpc-id $VPC_ID
