1f716611f1a9720753bf5e9a3bb46c6a42bc6718
cdb3832 <cdb3832@rit.edu>
2022-10-31 19:22:25 -0400

    fix: add proper logic in lambda function
    Properly retreive last 3 days of info from our dynamo db.

--- iac/iac-team-6.tf
+++ iac/iac-team-6.tf
@@ -241,7 +241,7 @@ resource "aws_dynamodb_table" "Hashtag-table-514-team6" {
   
   attribute {
     name = "searchDate"
-    type = "S"
+    type = "N"
   }
   
 }
@@ -285,7 +285,7 @@ EOF
 resource "aws_lambda_function" "retreive-rekognition-data" {
   function_name = "get-rekognition-data"
   role = aws_iam_role.iam_for_lambda.arn
-  filename = "rekognition-dynamo-lambda.zip"
+  filename = "retreive-dynamo-info.zip"
   runtime = "python3.8"
   handler = "lambda_function.lambda_handler"
 
