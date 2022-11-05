a66e8b54161c8a659d6608923bd44c10e86c5ac4
JorgeLFigueroa <figueroajorge010@gmail.com>
2022-11-05 15:19:58 -0400

    removed an extra method

--- iac/iac-team-6.tf
+++ iac/iac-team-6.tf
@@ -126,21 +126,6 @@ resource "aws_api_gateway_rest_api" "api" {
  description = "Proxy to handle requests to our API"
 }
 
-resource "aws_api_gateway_resource" "rekognition_data_resource" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "rekognitionData"
-}
-
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.rekognition_data_resource.id
-  http_method   = "GET"
-  authorization = "NONE"
-#  request_parameters = {
-#    "method.request.path.proxy" = true
-#  }
-}
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.rekognition_data_resource.id
@@ -155,6 +140,12 @@ resource "aws_api_gateway_integration" "integration" {
 #  request_parameters = {
 #    "method.request.path.proxy" = true
 #  }
+}
+
+resource "aws_api_gateway_resource" "rekognition_data_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "rekognitionData"
 }
 
 resource "aws_api_gateway_resource" "search_resource" {
