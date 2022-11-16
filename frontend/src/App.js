82976dadbfd75ff24c4670c0d111684940365d48
JorgeLFigueroa <figueroajorge010@gmail.com>
2022-11-15 22:26:52 -0500

    restoring topic in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import RenderResults from './components/RenderResults';
+import Topic from './components/Topic';
 import './styling/App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <RenderResults/>
+      <Topic name='news'/>
     </div>
   );
 }
